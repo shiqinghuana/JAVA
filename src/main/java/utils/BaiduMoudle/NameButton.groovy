fba373c5d183098d2b682fd55ed72e649bf57552
v993611@163.com <https://mail.163.com/>
2019-10-24 23:23:00 +0800

    geb

--- src/main/java/utils/BaiduMoudle/NameButton.groovy
+++ src/main/java/utils/BaiduMoudle/NameButton.groovy
@@ -1,4 +1,49 @@
 package utils.BaiduMoudle
 
-class NameButton {
+import geb.navigator.EmptyNavigator
+import geb.navigator.Navigator
+import groovy.util.logging.Slf4j
+
+@Slf4j
+class NameButton  extends StableEnhancedMoudle {
+    HashMap historydata = new HashMap<String, ArrayList<Navigator>>()
+    static content = {
+
+        /**
+         * 页面几种常见元素
+         * <a href 链接
+         * < input 输入框
+         *  <button
+         */
+        Link { $("a") }
+        Input { $("input") }
+        Button { $("button") }
+
+    }
+
+    Navigator clickWebElementByText(String name, Integer index=0) {
+
+            for (nameniv in Link) {
+                if (nameniv.text()?.contains(name)) {
+                    log.info("=查询到Link类型${name}")
+                   return nameniv
+
+                }
+
+            }
+            for (nameniv in Input) {
+                if (nameniv.text()?.contains(name)) {
+                    log.info("=查询到Input类型${name}")
+                    return nameniv
+                }
+
+            }
+            for (nameniv in Button) {
+                if (nameniv.text()?.contains(name)) {
+                    log.info("=查询到Button类型${name}")
+                    return nameniv
+                }
+
+            }
+    }
 }
--- src/main/java/utils/BaiduPage/BaiduUntilsCollection.groovy
+++ src/main/java/utils/BaiduPage/BaiduUntilsCollection.groovy
@@ -1,8 +1,11 @@
 package utils.BaiduPage
 
+import geb.Browser
 import geb.Page
 import geb.navigator.Navigator
 import groovy.util.logging.Slf4j
+import org.openqa.selenium.chrome.ChromeDriver
+import utils.BaiduMoudle.NameButton
 
 
 /**
@@ -10,6 +13,8 @@ import groovy.util.logging.Slf4j
  */
 @Slf4j
 class BaiduUntilsCollection extends Page{
+
+
     /**
      *   y用户脚本中使用的导航，查找元素后，找到的元素都存在这里
      * */
@@ -20,18 +25,33 @@ class BaiduUntilsCollection extends Page{
      * */
     String originalWindow = null
     static at = {true}
+    static url = "https://www.baidu.com"
+    static content = {
 
-
-
-
-    void ClickName(String Name){
-
-        //先查buttom
-
-        button = $("Button",text)
-
+        nameButton {module(NameButton)}
 
     }
 
 
+    void ClickName(String Name,Integer index=0){
+
+
+        //先查buttom
+
+        /*    button = $("Button",text)*/
+        Navigator niv =  nameButton.clickWebElementByText(Name,index)
+        if (niv){nameButton.stableClick(niv)}
+        else {log.info("页面没找到元素")}
+    }
+
+    public static void main(String[] args) {
+        Browser browser = new Browser(driver: new ChromeDriver())
+
+        Browser.drive (browser){
+            to BaiduUntilsCollection
+            sleep(5)
+            ClickName("新闻",)
+           // quit()
+        }
+    }
 }
